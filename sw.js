bcbbac10ccb35b1527463ca67b1bc07340e51268
Robert Albino <99913125+robertbr123@users.noreply.github.com>
2026-02-18 18:12:27 -0500

    up 29.2

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Ondeline Tech - App do Técnico
  */
 
-const APP_VERSION = 'v29.1';
+const APP_VERSION = 'v29.2';
 const CACHE_NAME = `ondeline-tech-${APP_VERSION}`;
 const STATIC_CACHE = `ondeline-static-${APP_VERSION}`;
 const DYNAMIC_CACHE = `ondeline-dynamic-${APP_VERSION}`;
